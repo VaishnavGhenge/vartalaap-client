ed8fa64926cc4be0a1e70a93e7cbbd6aec1be4fb
Vaishnav Ghenge <62459983+VaishnavGhenge@users.noreply.github.com>
2023-12-15 19:57:38 +0000

    Rename and Refactor userPreferences

---
+++ utils/persisitUserPreferences.ts
@@ -0,0 +1,18 @@
+export type UserPreferences = {
+    micStatus: boolean;
+    cameraStatus: boolean;
+}
+
+const setMeetPreferences = (preferences: UserPreferences): void => {
+    localStorage.setItem('meetPreferences', JSON.stringify({...getMeetPreferences(), ...preferences}));
+}
+
+const getMeetPreferences = (): UserPreferences => {
+    const storedPreferencesString = localStorage.getItem('meetPreferences') || null;
+    const meetPreferences = storedPreferencesString ? JSON.parse(storedPreferencesString) : {micStatus: true, cameraStatus: true} as UserPreferences
+    return meetPreferences;
+}
+
+const exports = {setMeetPreferences, getMeetPreferences}
+
+export default exports;
