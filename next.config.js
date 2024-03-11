7d3be39362a5fcd293c7a67828d933c0f4291d0f
Vaishnav Ghenge <vaishnavghenge@gmail.com>
2024-03-12 01:04:33 +0530

    Update: config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+      PROD_SERVER: process.env.PROD_SERVER || 'localhost:8080',
+    },
+}
 
 module.exports = nextConfig
